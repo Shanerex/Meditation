f644f5ce8a4d801570ab7fdf20fe38fb6ef21c59
Shane Rex <shanerex2001@gmail.com>
2021-08-08 22:35:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/meditation/ui/theme/Theme.kt
@@ -0,0 +1,19 @@
+package com.example.meditation.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun MeditationTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
